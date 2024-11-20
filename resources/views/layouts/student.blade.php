61098788e38d03fcd0a39925f911510c6a570a89
hafiyaiman1998 <hafiyyaimann1998@gmail.com>
2024-11-20 15:37:14 +0800

    Update student.blade.php

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -348,6 +348,7 @@
 				{{-- <li>
 					<a href="/yuran-pengajian" class=""><i data-feather="shopping-bag"></i><span>Payment</span></a>
 				</li> --}}
+				
 				<li>
 					<a href="{{ asset('storage/finals_schedule/Jadual Pengawasan Peperiksaan Akhir UNITI Kemasukan Julai 20242025-I.pdf') }}" target="_blank"><i data-feather="layout"></i><span>Finals Schedule</span></a>
 				</li> 
