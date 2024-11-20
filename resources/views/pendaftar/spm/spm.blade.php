15da8b57919d6ecea18853ccb6771b7e48fbe5c7
hafiyaiman1998 <hafiyyaimann1998@gmail.com>
2024-11-20 15:35:00 +0800

    Update spm.blade.php

--- resources/views/pendaftar/spm/spm.blade.php
+++ resources/views/pendaftar/spm/spm.blade.php
@@ -204,6 +204,7 @@
                                     </div>
                                 </div>
                             </div>
+                            
                             <div class="col-md-12">
                                 <div class="row">
                                     <div class="col-md-5">
